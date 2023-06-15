a3177fc22135dacd63ba0556eea95cb09a65cd5d
emhamza <imumairhamza@gmail.com>
2023-06-15 06:46:32 -0700

    Create Morse decoder

---
+++ morse_code_dec.rb
@@ -0,0 +1,50 @@
+MORSE_CODE_DICT = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE_DICT[morse_char.upcase]
+end
+
+puts decode_char('.--')
+
+def decode_word(morse_word)
+  morse_chars = morse_word.split
+  morse_chars.map { |char| decode_char(char) }.join
+end
+
+puts decode_char('A')
+
+def decode_message(morse_message)
+  morse_words = morse_message.split
+  morse_words.map { |word| decode_word(word) }.join
+end
+
+morse_message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = decode_message(morse_message)
+puts decoded_message
